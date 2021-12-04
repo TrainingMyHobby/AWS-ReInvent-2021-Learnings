a58a493bd75b2c599da222d2508546d6d48dd805
Training My Hobby <training.my.hobby@gmail.com>
2021-12-04 18:51:22 -0500

    Initial Commit

--- git_push.sh
+++ git_push.sh
@@ -1,4 +1,6 @@
-git config user.name TrainingMyHobby
-git config user.email training.my.hobby@gmail.com
+# git config user.name TrainingMyHobby
+# git config user.email training.my.hobby@gmail.com
 # git config --local credential.helper ""
-git push origin main
+# git push origin main
+
+git remote set-url origin https://${GITHUB_TMH_TOKEN}@github.com/TrainingMyHobby/AWS-ReInvent-2021-Learnings.git
